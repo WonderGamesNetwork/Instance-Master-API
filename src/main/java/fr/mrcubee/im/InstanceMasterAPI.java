c129c248ed0240b804da561ab469e8ed2045ef0b
MrCubee <contact@mrcubee.fr>
2023-11-03 12:35:00 +0100

    Add getInstance method to get API instance.

--- src/main/java/fr/mrcubee/im/InstanceMasterAPI.java
+++ src/main/java/fr/mrcubee/im/InstanceMasterAPI.java
@@ -1,5 +1,30 @@
 package fr.mrcubee.im;
 
+import net.md_5.bungee.api.plugin.Plugin;
+
+import java.lang.reflect.Field;
+
 public interface InstanceMasterAPI {
+
+    private static Plugin getClassPlugin(final Class<?> clazz) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException, ClassCastException {
+        final Class<?> classPluginClassLoader = Class.forName("net.md_5.bungee.api.plugin.PluginClassloader");
+        final ClassLoader classLoader = clazz.getClassLoader();
+        final Field pluginField;
+
+        if (!classPluginClassLoader.isInstance(classLoader))
+            return null;
+        pluginField = classLoader.getClass().getField("plugin");
+        pluginField.setAccessible(true);
+        return (Plugin) pluginField.get(classLoader);
+    }
+
+    public static InstanceMasterAPI getInstance() {
+        Plugin plugin = null;
+
+        try {
+            plugin = getClassPlugin(InstanceMasterAPI.class);
+        } catch (Exception ignored) {}
+        return (plugin instanceof InstanceMasterAPI) ? (InstanceMasterAPI) plugin : null;
+    }
 
 }
